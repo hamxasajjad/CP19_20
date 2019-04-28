320617302f4594bd240f8684d3ca13fe072a81ba
zeeshanwasi1 <zeeshan1998>
2019-04-28 11:50:06 -0700

    zeeshan GUI

---
+++ hello.py
@@ -0,0 +1,23 @@
+from Tkinter import *
+
+def donothing():
+   x = 0
+   
+root = Tk()
+
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="New", command=donothing)
+filemenu.add_command(label="Open", command=donothing)
+filemenu.add_command(label="Save", command=donothing)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=root.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+
+helpmenu = Menu(menubar, tearoff=0)
+helpmenu.add_command(label="Help Index", command=donothing)
+helpmenu.add_command(label="About...", command=donothing)
+menubar.add_cascade(label="Help", menu=helpmenu)
+
+root.config(menu=menubar)
+root.mainloop()
