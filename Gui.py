da4e23e123ab25e88d55c709d9864500149228c0
Muhammad-Haris-Khan <sp19bsse0084@maju.edu.pk>
2019-04-28 09:32:01 -0700

    gui By Haris

---
+++ Gui.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[78]:
+
+
+from tkinter import *
+from tkinter import messagebox
+top = Tk()
+top.geometry("400x300")
+t=Label(top,text="Please Enter Your Name")
+e=Entry(top,bd=5,)
+t.grid(row=2,)
+e.grid(row=2,column=1)
+def message():
+   msg = messagebox.showinfo( "Hello", e.get())
+
+B = Button(top, text = "Submit", command = message,height=3,width=10,bg="gray",fg="white")
+B.place(x = 150,y = 100)
+B.flash()
+top.mainloop()
+
